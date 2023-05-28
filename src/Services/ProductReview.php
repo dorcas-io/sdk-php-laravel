294ca9486ee07a72ee0a9b09c1f517f6c1c5442e
Olawuyi <olawuyi@Olawuyis-MacBook-Pro.local>
2023-05-28 21:31:20 +0100

    added product reviews to sdk

---
+++ src/Services/ProductReview.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Hostville\Dorcas\Services;
+
+
+class ProductReview extends AbstractService
+{
+
+    /**
+     * Returns the name of the resource.
+     *
+     * @return string
+     */
+    function getName(): string
+    {
+        return 'ProductReview';
+    }
+}
